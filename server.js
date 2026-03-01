8757a9bba99f73ca23d3c6309afc8d1a93fd4fe5
swamyreddy <reddyswamy924@gmail.com>
2026-03-01 13:23:45 +0530

    added sync to sequelize

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 5000;
 
 sequelize
     .authenticate()
+    .sync({ alter: true })
     .then(() => console.log("DB connected"))
     .catch((err) => console.error(err));
 
